cf00d7ae70b9593fbece25ff066a8c887370dc06
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2024-10-08 18:49:01 +0300

    Started data preparation

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def read_csv(file_path):
+    if file_path.endswith('.csv'):
+        data = pd.read_csv(file_path)
+    else:
+        data = pd.read_excel(file_path)
+    return data
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Data Summary Function
+def Data_Summary(data):
+    print("Data Summary for file: ", file)
+    print("=====================================")
+    print("First 5 rows")
+    print(data.describe())
+    print("=====================================")
+    print("Data types")
+    print(data.info())
+    print("=====================================")
+    print("Data count")
+    print(data.count())
+    print("=====================================")
+    print("Missing values")
+    print(data.isnull().sum())
+    print("=====================================")
+    print("Data shape")
+    print(data.shape)
+    print("=====================================")
+    print("Unique values in each column")
+    print(data.nunique())
+    print("=====================================")
+
+    # describe the last column
+    print("Last column description")
+    # print(data.iloc[:, -1].describe())
+    # # Visualize the last column
+    # temp_data = data.drop(data.index[0])
+    # plt.figure(figsize=(7, 6))
+    # plt.bar(temp_data.iloc[:, -1].unique(), temp_data.iloc[:, -1].value_counts())
+    # plt.show(block=True)
+
+    print("---------------------------------------------------------------------------------")
+
+# Linear Interpolation but only for the columns with numerical values and same type of data
+def LinearInterpolation(data):
+    columns_names = ["bg-", "insulin-", "carbs-", "hr-", "steps-", "cals-"]
+    last_column_name = columns_names[0]
+
+    bg_df = data.filter(like='bg-', axis=1)
+    bg_df = bg_df.interpolate(method='linear', axis=0)
+
+    insulin_df = data.filter(like='insulin-', axis=1)
+    insulin_df = insulin_df.interpolate(method='linear', axis=0)
+
+    carbs_df = data.filter(like='carbs-', axis=1)
+    carbs_df = carbs_df.interpolate(method='linear', axis=0)
+
+    hr_df = data.filter(like='hr-', axis=1)
+    hr_df = hr_df.interpolate(method='linear', axis=0)
+
+    steps_df = data.filter(like='steps-', axis=1)
+    steps_df = steps_df.interpolate(method='linear', axis=0)
+
+    cals_df = data.filter(like='cals-', axis=1)
+    cals_df = cals_df.interpolate(method='linear', axis=0)
+
+    # the rest of the data that is not in the column names
+    the_rest_df = data.drop(columns=bg_df.columns)
+    the_rest_df = the_rest_df.drop(columns=insulin_df.columns)
+    the_rest_df = the_rest_df.drop(columns=carbs_df.columns)
+    the_rest_df = the_rest_df.drop(columns=hr_df.columns)
+    the_rest_df = the_rest_df.drop(columns=steps_df.columns)
+    the_rest_df = the_rest_df.drop(columns=cals_df.columns)
+
+    data = pd.concat([bg_df, insulin_df, carbs_df, hr_df, steps_df, cals_df, the_rest_df], axis=1)
+
+    return data
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    file = "Data/train.csv"
+    data = read_csv(file)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data = LinearInterpolation(data)
+
+    # save the data
+    data.to_csv("Data/train_interpolated.csv", index=False)
+
+    # Data_Summary(data)
